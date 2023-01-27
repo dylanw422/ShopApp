da74b63a8659981fe0af229db337af0596054e7d
Dylan <dylanw422@gmail.com>
2023-01-27 10:32:03 -0600

    Routing

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import RouteSwitch from './RouteSwitch';
 import './index.css'
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouteSwitch />
+    <HashRouter basename='/'>
+      <RouteSwitch />
+    </HashRouter>
   </React.StrictMode>
 );
 
