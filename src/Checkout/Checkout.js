939b7f7d5352b862b623b503be77852caf52d7c4
Dylan <dylanw422@gmail.com>
2023-02-01 11:33:43 -0600

    build

--- src/Checkout/Checkout.js
+++ src/Checkout/Checkout.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import './Checkout.css'
 import songData from '../Shop/songData.js'
 
--- src/Shop/Shop.js
+++ src/Shop/Shop.js
@@ -1,5 +1,5 @@
 //import React
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import './Shop.css'
 import songData from './songData.js'
 
